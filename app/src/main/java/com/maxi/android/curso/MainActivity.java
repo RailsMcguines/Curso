8149cfbe2935db26bf8bf71a6689388f446ef923
Maxi <maxi_ruiz140@hotmail.com>
2017-11-03 22:13:24 -0300

    Initial commit

---
+++ app/src/main/java/com/maxi/android/curso/MainActivity.java
@@ -0,0 +1,22 @@
+package com.maxi.android.curso;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.maxi.android.curso.activity.LineraLayoutActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchLinearLayoutActivity(View view){
+        Intent intent = new Intent(this, LineraLayoutActivity.class);
+        startActivity(intent);
+    }
+}
