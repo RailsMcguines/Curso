c0e890d221a05f1459084b8882b6c27fd09d14b7
Maxi Ruiz <maxi.ruiz140@gmail.com>
2017-11-04 11:50:06 -0300

    usar listeneres

--- app/src/main/java/com/maxi/android/curso/MainActivity.java
+++ app/src/main/java/com/maxi/android/curso/MainActivity.java
@@ -4,20 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.maxi.android.curso.activity.CollapsingToolBarActivity;
 import com.maxi.android.curso.activity.LineraLayoutActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1, btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchLinearLayoutActivity(v);
+            }
+        });
+
+        btn1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                launchToast(v);
+                return true;
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCollapsingToolBarActivity(v);
+            }
+        });
     }
 
     public void launchLinearLayoutActivity(View view){
-        Intent intent = new Intent(this, LineraLayoutActivity.class);
-        startActivity(intent);
+        Intent i = new Intent(this, LineraLayoutActivity.class);
+        startActivity(i);
     }
 
     public void launchCollapsingToolBarActivity(View view){
@@ -26,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchToast(View view){
-        Intent intent = new Intent(this, CollapsingToolBarActivity.class);
-        startActivity(intent);
+        Toast.makeText(this, "Esto es un Toast", Toast.LENGTH_LONG).show();
     }
 }
